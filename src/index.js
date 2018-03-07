85503523e6be91dd96fcef4ddffbd3d0145539cb
Mark Conlon <mconlon@astoundgroup.com>
2018-03-07 12:45:33 -0500

    Videos 2 to 3 getting setup , importing rendering, exporting

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-// let's go!
-alert("hey");
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+// component to be rendered and where to mount it to
+render(<StorePicker/>, document.querySelector('#main'));
