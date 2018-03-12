02f59466bf890b683c6fb18d2b3cdc45593435ee
Mark Conlon <mconlon@astoundgroup.com>
2018-03-12 12:54:07 -0400

    emmett for JSX in react and render options

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>Hello!</p>
+        // you don't have to write JSX you can use this instead:
+        // return React.createElement('p', { className: 'hey'})
+        return <form class="store-selector"></form>
     }
 }
 
