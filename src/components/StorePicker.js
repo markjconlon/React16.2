11e3107ae201f35a74b226cd52832c1d81158eeb
Mark Conlon <mconlon@astoundgroup.com>
2018-03-12 13:04:32 -0400

    updated settings for editor and finished the Store Picker form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,7 +4,16 @@ class StorePicker extends React.Component {
     render() {
         // you don't have to write JSX you can use this instead:
         // return React.createElement('p', { className: 'hey'})
-        return <form class="store-selector"></form>
+        return (
+            <React.Fragment>
+                {/* This is how you write a comment it also has to be inside an element */}
+                <form className="store-selector">
+                    <h2>Please Enter A Store</h2>
+                    <input type="text" required placeholder="Store Name"/>
+                    <button type="submit">Visit Store -></button>
+                </form>
+            </React.Fragment>
+        )
     }
 }
 
